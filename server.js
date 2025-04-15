119d2921b9f70463a8ef0c1bc801a8f3ef5f62c9
Volanalytics <bpmoran@protonmail.com>
2025-04-15 13:30:00 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2086,7 +2086,7 @@ app.get('/share/:id', async (req, res) => {
           
           <div class="buttons">
             <a href="${safeUrl}" class="button">Read Full Article</a>
-            <a href="https://tennesseefeeds.com/dev.html?article=${articleBlockId}" class="button" style="background-color: #666;">View on TennesseeFeeds</a>
+            <a href="https://tennesseefeeds.com" class="button" style="background-color: #666;">Go to TennesseeFeeds</a>
           </div>
           
           <p class="redirect-message">You will be redirected to the article in <span id="countdown">5</span> seconds...</p>
